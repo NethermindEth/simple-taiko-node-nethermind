5060f6ec30d3a64302159d22338fbfe56ef570ac
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2026-07-17 13:26:32 +0200

    config Catalyst to use mtls for web3signer connection

--- mtls/generate.sh
+++ mtls/generate.sh
@@ -1,3 +1,24 @@
+create_cnf_file() {
+    local name="$1"
+
+    cat > "${name}.cnf" <<EOF
+[req]
+prompt = no
+distinguished_name = dn
+req_extensions = req_ext
+
+[dn]
+CN = ${name}
+
+[req_ext]
+subjectAltName = @alt_names
+extendedKeyUsage = serverAuth
+
+[alt_names]
+DNS.1 = ${name}
+EOF
+}
+
 #Create a Certificate Authority
 openssl genrsa -out ca.key 4096
 
@@ -8,49 +29,109 @@ openssl req -x509 -new -nodes \
     -out ca.crt \
     -subj "/CN=Web3Signer CA"
 
-#Generate the Web3Signer server certificate
-openssl genrsa -out server.key 4096
+#Generate the Web3Signer L1 server certificate
+openssl genrsa -out web3signer_l1.key 4096
 
-openssl req -new \
-    -key server.key \
-    -out server.csr \
-    -subj "/CN=web3signer"
+create_cnf_file "web3signer_l1"
 
-openssl x509 -req \
-    -in server.csr \
-    -CA ca.crt \
-    -CAkey ca.key \
-    -CAcreateserial \
-    -out server.crt \
-    -days 365 \
-    -sha256
+openssl req \
+  -new \
+  -key web3signer_l1.key \
+  -out web3signer_l1.csr \
+  -config web3signer_l1.cnf
+
+openssl x509 \
+  -req \
+  -in web3signer_l1.csr \
+  -CA ca.crt \
+  -CAkey ca.key \
+  -CAcreateserial \
+  -out web3signer_l1.crt \
+  -days 365 \
+  -sha256 \
+  -extensions req_ext \
+  -extfile web3signer_l1.cnf
 
 #Convert to PKCS12
-echo $(openssl rand -base64 32) > server.password
+# TODO improve password generation, currently it is just a random string
+echo $(openssl rand -base64 32) > web3signer_l1.password
 
-openssl pkcs12 -export \
-    -inkey server.key \
-    -in server.crt \
-    -certfile ca.crt \
-    -out server.p12 \
-    -passout file:server.password
+openssl pkcs12 \
+  -export \
+  -inkey web3signer_l1.key \
+  -in web3signer_l1.crt \
+  -certfile ca.crt \
+  -out web3signer_l1.p12 \
+  -passout file:web3signer_l1.password
+
+#Generate the Web3Signer L2 server certificate
+openssl genrsa -out web3signer_l2.key 4096
+
+create_cnf_file "web3signer_l2"
+
+openssl req \
+  -new \
+  -key web3signer_l2.key \
+  -out web3signer_l2.csr \
+  -config web3signer_l2.cnf
+
+openssl x509 \
+  -req \
+  -in web3signer_l2.csr \
+  -CA ca.crt \
+  -CAkey ca.key \
+  -CAcreateserial \
+  -out web3signer_l2.crt \
+  -days 365 \
+  -sha256 \
+  -extensions req_ext \
+  -extfile web3signer_l2.cnf
+
+#Convert to PKCS12
+echo $(openssl rand -base64 32) > web3signer_l2.password
+
+openssl pkcs12 \
+  -export \
+  -inkey web3signer_l2.key \
+  -in web3signer_l2.crt \
+  -certfile ca.crt \
+  -out web3signer_l2.p12 \
+  -passout file:web3signer_l2.password
+
 
 #Generate the Catalyst client certificate
-openssl genrsa -out client.key 4096
+openssl genrsa -out catalyst.key 4096
 
-openssl req -new \
-    -key client.key \
-    -out client.csr \
-    -subj "/CN=catalyst"
+cat > "catalyst.cnf" <<EOF
+[req]
+prompt = no
+distinguished_name = dn
+req_extensions = req_ext
 
-openssl x509 -req \
-    -in client.csr \
-    -CA ca.crt \
-    -CAkey ca.key \
-    -CAcreateserial \
-    -out client.crt \
-    -days 365 \
-    -sha256
+[dn]
+CN = catalyst
+
+[req_ext]
+extendedKeyUsage = clientAuth
+EOF
+
+openssl req \
+  -new \
+  -key catalyst.key \
+  -out catalyst.csr \
+  -config catalyst.cnf
+
+openssl x509 \
+  -req \
+  -in catalyst.csr \
+  -CA ca.crt \
+  -CAkey ca.key \
+  -CAcreateserial \
+  -out catalyst.crt \
+  -days 365 \
+  -sha256 \
+  -extensions req_ext \
+  -extfile catalyst.cnf
 
 #Create knownClients
-cp client.crt knownClients
+cp catalyst.crt knownClients
